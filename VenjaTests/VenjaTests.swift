2f68b4f6cc956b8c3a45325c1c58dcc448ad95ff
Anders Hovmöller <anders.hovmoller@dryft.se>
2025-06-08 17:26:30 +0200

    Initial Commit

---
+++ VenjaTests/VenjaTests.swift
@@ -0,0 +1,16 @@
+//
+//  VenjaTests.swift
+//  VenjaTests
+//
+//  Created by Anders Hovmöller on 2025-06-08.
+//
+
+import Testing
+
+struct VenjaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
